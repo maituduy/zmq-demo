eefe2b461005d457bc6356a8047776af03a545db
maituduy <maituduy1998@gmail.com>
2019-10-13 15:55:07 +0700

    f

---
+++ client/build/CMakeFiles/hwclient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwclient.dir/hwclient.cpp.obj"
+  "hwclient.exe"
+  "hwclient.exe.manifest"
+  "hwclient.pdb"
+  "libhwclient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hwclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
